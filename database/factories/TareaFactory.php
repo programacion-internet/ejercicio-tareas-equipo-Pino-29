832d4f16052cb318e682584a7b0dd9ec34020580
Pino-29 <roberto.g.pino@gmail.com>
2025-05-08 14:07:55 -0600

    Add 30% chance for generating overdue Tareas

--- database/factories/TareaFactory.php
+++ database/factories/TareaFactory.php
@@ -17,10 +17,14 @@ class TareaFactory extends Factory
      */
     public function definition(): array
     {
+        $isPast = $this->faker->boolean(30); // 30% chance of being in the past
+
         return [
             'nombre'        => $this->faker->sentence,
             'descripcion'   => $this->faker->paragraph,
-            'fecha_limite' => $this->faker->dateTimeBetween('now', '+1 month'),
+            'fecha_limite'  => $isPast
+                ? $this->faker->dateTimeBetween('-1 month', 'now')
+                : $this->faker->dateTimeBetween('now', '+1 month'),
             'user_id'       => User::inRandomOrder()->first()?->id ?? User::factory(),
         ];
     }
